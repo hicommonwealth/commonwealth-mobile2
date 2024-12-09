1cdd35a0d2924af338d46f01dfe063c554a28ca2
Kevin Burton <burtonator@gmail.com>
2024-12-09 12:34:00 -0800

    online and offline detection works...

---
+++ app/Offline.tsx
@@ -0,0 +1,28 @@
+import {View, Text} from "react-native";
+import React from "react";
+
+export const Offline = () => {
+  return (
+    <View style={styles.centered}>
+      <Text style={styles.offlineText}>You are offline!</Text>
+      <Text style={styles.instructions}>Please check your internet connection.</Text>
+    </View>
+  )
+}
+
+const styles = {
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  offlineText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  instructions: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+};
