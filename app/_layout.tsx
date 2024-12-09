18ee4f08485d3c32e31d5cb77d2cd1d7f626fd67
Kevin Burton <burtonator@gmail.com>
2024-12-09 14:20:46 -0800

    This is needed to get rid of the header.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
-import { Stack } from "expo-router";
+import {Slot} from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Slot />
+      {/*<StatusBar style="auto" />*/}
+    </>
+  )
+
 }
