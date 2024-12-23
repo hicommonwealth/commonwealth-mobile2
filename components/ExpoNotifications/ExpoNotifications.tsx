2c5de6a7820d19174dbd08a9d4395bed08475676
Kevin Burton <burtonator@gmail.com>
2024-12-23 11:52:56 -0800

    we now support receiving notifications but we're not properly handling the link ... that will need to be done next.

--- components/ExpoNotifications/ExpoNotifications.tsx
+++ components/ExpoNotifications/ExpoNotifications.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {
   KnockExpoPushNotificationProvider,
   KnockProvider,
 } from "@knocklabs/expo";
 import { KNOCK_PUBLIC_API_KEY, KNOCK_EXPO_CHANNEL_ID } from '@env';
+import * as Notifications from 'expo-notifications';
+import {
+  NotificationHandler
+} from "@/components/ExpoNotifications/NotificationHandler";
 
 type Props = {
   userId: number
   knockJWT: string
 }
 
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 // https://docs.knock.app/sdks/expo/push-notifications
 // https://docs.knock.app/integrations/push/expo#how-to-configure-expo-with-knock
 // https://docs.knock.app/sdks/react-native/push-notifications
+// https://docs.knock.app/integrations/push/expo
 
 // https://docs.expo.dev/push-notifications/sending-notifications-custom/
 
@@ -29,6 +43,7 @@ export default function ExpoNotifications(props: Props) {
                    userToken={props.knockJWT}>
       <KnockExpoPushNotificationProvider knockExpoChannelId={KNOCK_EXPO_CHANNEL_ID}>
         <>
+          <NotificationHandler/>
         </>
       </KnockExpoPushNotificationProvider>
     </KnockProvider>
---
+++ components/ExpoNotifications/NotificationHandler.tsx
@@ -0,0 +1,72 @@
+import {useEffect} from "react";
+import { useExpoPushNotifications } from "@knocklabs/expo";
+import * as Notifications from "expo-notifications";
+import {Linking} from "react-native";
+
+export const NotificationHandler = () => {
+
+  const {onNotificationReceived, onNotificationTapped} = useExpoPushNotifications()
+
+  useEffect(() => {
+    onNotificationReceived((notification) => {
+
+      const event_data = notification.request.content.data
+
+      if (! event_data) {
+        console.warn("No event data found in notification")
+        return;
+      }
+
+      console.log("event_data", JSON.stringify(event_data, null, 2))
+
+      console.log("Notification received:", JSON.stringify(notification, null, 2));
+
+      const title = event_data.notification?.title || event_data.title || 'No title';
+      const body =
+        event_data.notification?.body ||
+        event_data.body ||
+        event_data.comment_body ||
+        'No body';
+      
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title,
+          body,
+          sound: 'default',
+        },
+        trigger: null,
+      }).catch(console.error);
+
+    });
+
+    onNotificationTapped((response) => {
+      // handle linking here...
+      console.log("Notification tapped:", JSON.stringify(response, null, 2));
+
+      const event_data = response.notification.request.content;
+
+      if (! event_data) {
+        console.warn("No event data found in notification")
+        return;
+      }
+
+      const url =
+        event_data?.data.notification?.url ||
+        event_data?.data?.url ||
+        event_data?.data?.comment_url ||
+        event_data?.data?.object_url ||
+        event_data?.data?.community_stakes_url ||
+        'https://common.xyz';
+
+      // TODO: OK this is SUPER hard to fix now and we're going to have to
+      // take this URL, then push it into the webapp and have an updated router
+      // there than adapts the URL and changes it for us. However, we need this
+      // for deep linking anyway.
+      Linking.openURL(url).catch(console.error);
+
+    });
+  }, []);
+
+  return null;
+
+};
