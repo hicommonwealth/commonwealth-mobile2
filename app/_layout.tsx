4f86fe0f7e2d311fe15704136344c0b581c93d25
Kevin Burton <burtonator@gmail.com>
2025-02-25 12:59:25 -0800

    ...

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,18 +3,23 @@ import {SafeAreaProvider} from "react-native-safe-area-context";
 import NetworkDetector from "@/components/NetworkDetector/NetworkDetector";
 import {interceptLogging} from "@/util/interceptLogging";
 import {Remounter} from "@/components/Remounter/Remounter";
+import {Debug} from "@/components/Debug/Debug";
 
 interceptLogging()
 
 export default function RootLayout() {
   return (
-    <Remounter>
-
-      <SafeAreaProvider>
-        <NetworkDetector/>
-      </SafeAreaProvider>
-
-    </Remounter>
+    <Debug/>
   )
+
+  // return (
+  //   <Remounter>
+  //
+  //     <SafeAreaProvider>
+  //       <NetworkDetector/>
+  //     </SafeAreaProvider>
+  //
+  //   </Remounter>
+  // )
 
 }
