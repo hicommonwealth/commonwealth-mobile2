fb00af5f245568935a004c7e52633720fdb841aa
Kevin Burton <burtonator@gmail.com>
2025-02-26 10:43:22 -0800

    In theory this should fix the offline error...

--- components/Offline/Offline.tsx
+++ components/Offline/Offline.tsx
@@ -1,17 +1,21 @@
 import {View, Text} from "react-native";
 import React from "react";
+import SafeAreaContainer from "@/components/SafeAreaContainer/SafeAreaContainer";
 
 export default function Offline() {
   return (
-    <View style={styles.centered}>
-      <Text style={styles.offlineText}>You are offline!</Text>
-      <Text style={styles.instructions}>Please check your internet connection.</Text>
-    </View>
+    <SafeAreaContainer darkMode={false}>
+      <View style={styles.centered}>
+        <Text style={styles.offlineText}>You are offline!</Text>
+        <Text style={styles.instructions}>Please check your internet connection.</Text>
+      </View>
+    </SafeAreaContainer>
   )
 }
 
 const styles = {
   centered: {
+    backgroundColor: 'white',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
