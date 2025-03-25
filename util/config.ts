f65e95c161563a6855ec136de4b248b428ae5aac
Kevin Burton <burtonator@gmail.com>
2025-03-25 11:09:39 -0700

    removed magic because it's no longer needed

--- util/config.ts
+++ util/config.ts
@@ -6,7 +6,6 @@ export type Config = {
   MAIN_APP_URL: string
   KNOCK_EXPO_CHANNEL_ID: string
   KNOCK_PUBLIC_API_KEY: string
-  MAGIC_PUBLISHABLE_KEY: string
 }
 
 const PROD_CONFIG: Config = {
@@ -15,7 +14,6 @@ const PROD_CONFIG: Config = {
   MAIN_APP_URL: 'https://common.xyz/',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_ynCCqD_rlxXTO0TvBCYUKYV5BSG5-vHoy451WGDHW5w",
-  MAGIC_PUBLISHABLE_KEY: 'pk_live_EF89AABAFB87D6F4'
 }
 
 const FRACK_CONFIG: Config = {
@@ -23,7 +21,6 @@ const FRACK_CONFIG: Config = {
   MAIN_APP_URL: 'https://commonwealth-frack.herokuapp.com/mobile-signin',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_EkjqgrIByZo85tIqdBkCmihVBtTB_ixY_37oTG_Au1Y",
-  MAGIC_PUBLISHABLE_KEY: 'pk_live_EF89AABAFB87D6F4'
 }
 
 const BETA_CONFIG: Config = {
@@ -31,7 +28,6 @@ const BETA_CONFIG: Config = {
   MAIN_APP_URL: 'https://common.xyz/',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_RLg22EIJ6jsuci6c7VvBU59gDQJZeFoeBKlOkgJLWvA",
-  MAGIC_PUBLISHABLE_KEY: 'pk_live_EF89AABAFB87D6F4'
 }
 
 
