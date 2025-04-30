c1a0bb1e6938837433006c9cb7bbf83b963c7fbd
Kevin Burton <burtonator@gmail.com>
2025-04-30 10:21:27 -0700

    privy polyfills... and privy config

--- util/config.ts
+++ util/config.ts
@@ -6,6 +6,8 @@ export type Config = {
   MAIN_APP_URL: string
   KNOCK_EXPO_CHANNEL_ID: string
   KNOCK_PUBLIC_API_KEY: string
+  PRIVY_APP_ID: string;
+  PRIVY_CLIENT_ID: string;
 }
 
 const PROD_CONFIG: Config = {
@@ -14,6 +16,9 @@ const PROD_CONFIG: Config = {
   MAIN_APP_URL: 'https://common.xyz/',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_ynCCqD_rlxXTO0TvBCYUKYV5BSG5-vHoy451WGDHW5w",
+  PRIVY_APP_ID: 'cm8er6mrm00fowbqy8bpw3956',
+  PRIVY_CLIENT_ID: 'client-WY5i1Up82Zaa6pQzZqFQHHLGxnuXQkeik7oCbhMW1AwQC'
+
 }
 
 const FRACK_CONFIG: Config = {
@@ -21,6 +26,8 @@ const FRACK_CONFIG: Config = {
   MAIN_APP_URL: 'https://commonwealth-frack.herokuapp.com',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_EkjqgrIByZo85tIqdBkCmihVBtTB_ixY_37oTG_Au1Y",
+  PRIVY_APP_ID: 'cm8er6mrm00fowbqy8bpw3956',
+  PRIVY_CLIENT_ID: 'client-WY5i1Up82Zaa6pQzZqFQHHLGxnuXQkeik7oCbhMW1AwQC'
 }
 
 const BETA_CONFIG: Config = {
@@ -28,6 +35,8 @@ const BETA_CONFIG: Config = {
   MAIN_APP_URL: 'https://beta.commonwealth.im',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_RLg22EIJ6jsuci6c7VvBU59gDQJZeFoeBKlOkgJLWvA",
+  PRIVY_APP_ID: 'cm8er6mrm00fowbqy8bpw3956',
+  PRIVY_CLIENT_ID: 'client-WY5i1Up82Zaa6pQzZqFQHHLGxnuXQkeik7oCbhMW1AwQC'
 }
 
 const TEST_CONFIG: Config = {
@@ -35,6 +44,8 @@ const TEST_CONFIG: Config = {
   MAIN_APP_URL: 'https://iframetester.com/?url=https://example.com',
   KNOCK_EXPO_CHANNEL_ID: "c416d699-c6ac-4288-8d76-9a792cf53ffa",
   KNOCK_PUBLIC_API_KEY: "pk_RLg22EIJ6jsuci6c7VvBU59gDQJZeFoeBKlOkgJLWvA",
+  PRIVY_APP_ID: 'cm8er6mrm00fowbqy8bpw3956',
+  PRIVY_CLIENT_ID: 'client-WY5i1Up82Zaa6pQzZqFQHHLGxnuXQkeik7oCbhMW1AwQC'
 }
 
 export function setConfig(conf: ConfigName) {
