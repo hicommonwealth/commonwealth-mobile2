fff4ac22d72749d7664e0fb7cf73eb2d495530d8
Kevin Burton <burtonator@gmail.com>
2025-05-01 11:39:47 -0700

    sign message works...

--- components/RequireAuth/AuthDebug.tsx
+++ components/RequireAuth/AuthDebug.tsx
@@ -1,16 +1,28 @@
 import {usePrivy} from "@privy-io/expo";
 import {View, Text, Button} from "react-native";
+import {useSignMessage} from "@privy-io/expo/ui";
 
 export const AuthDebug = () => {
   const {user, logout} = usePrivy();
+  const {signMessage} = useSignMessage()
 
   const handleLogout = () => {
     logout().catch(console.error)
   }
 
+  const handleSignMessage = () => {
+    async function doAsync() {
+      await signMessage({message: 'hello world'});
+
+    }
+    doAsync().catch(console.error);
+  }
+
   return (
     <View>
       <Text>Logged in as user: {user?.id}</Text>
+
+      <Button title="Sign Message" onPress={handleSignMessage}/>
 
       <Button title="Logout" onPress={handleLogout}/>
     </View>
