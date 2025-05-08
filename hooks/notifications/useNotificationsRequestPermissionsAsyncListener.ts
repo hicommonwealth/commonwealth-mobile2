dda5ed73994f146a1a6482e7c78a8e630fb7d7ce
Kevin Burton <burtonator@gmail.com>
2025-05-08 13:01:24 -0700

    return the value...

--- hooks/notifications/useNotificationsRequestPermissionsAsyncListener.ts
+++ hooks/notifications/useNotificationsRequestPermissionsAsyncListener.ts
@@ -2,11 +2,15 @@ import {useCallback} from "react";
 import {useMobileRPCReceiver} from "@/hooks/rpc/useMobileRPCReceiver";
 import * as Notifications from "expo-notifications";
 
+type PermissionStatus = {
+  status: 'granted' | 'denied' | 'undetermined'
+}
+
 export function useNotificationsRequestPermissionsAsyncListener() {
 
-  const handler = useCallback(async (request: {}): Promise<{}> => {
-    await Notifications.requestPermissionsAsync()
-    return {}
+  const handler = useCallback(async (request: {}): Promise<PermissionStatus> => {
+    const {status} = await Notifications.requestPermissionsAsync()
+    return {status}
   }, [])
 
   return useMobileRPCReceiver<{}, {}>('Notifications.requestPermissionsAsync', handler)
