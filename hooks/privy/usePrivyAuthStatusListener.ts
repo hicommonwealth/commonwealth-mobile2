d27670ee32b57c4071c9ed9fc799dff1713d8ea4
Kevin Burton <burtonator@gmail.com>
2025-05-12 12:39:19 -0700

    got auth to work but all this is ugly so I have to spend some time to clean everythign up...

--- hooks/privy/usePrivyAuthStatusListener.ts
+++ hooks/privy/usePrivyAuthStatusListener.ts
@@ -1,11 +1,14 @@
 import {useCallback} from "react";
 import {useMobileRPCReceiver} from "@/hooks/rpc/useMobileRPCReceiver";
-import {IPrivyAuthStatus, usePrivyAuthStatus} from "@/hooks/privy/usePrivyAuthStatus";
+import {usePrivyAuthStatus, IPrivyAuthStatus} from "@/components/RequireAuth/PrivyAuthContext";
 
 export function usePrivyAuthStatusListener() {
-  const privyAuthStatus = usePrivyAuthStatus();
+
+  const privyAuthStatus = usePrivyAuthStatus()
+
   const handler = useCallback(async (request: {}): Promise<IPrivyAuthStatus> => {
-    return await privyAuthStatus()
+    console.log("FIXME 666 returning privyAuthStatus: ", JSON.stringify(privyAuthStatus, null, 2))
+    return privyAuthStatus
   }, [privyAuthStatus])
 
   return useMobileRPCReceiver<{}, IPrivyAuthStatus>('privy.authStatus', handler)
